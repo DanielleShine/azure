d5b5cbb79c4794c27e2fdd95d40f3ac193a5baa7
DanielleShine <danielleshineabacial@gmail.com>
2025-03-10 14:29:44 +0800

    printing env var...again

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -21,12 +21,30 @@ print(os.environ.get('CLIENT_SECRET'))
 print(os.environ.get('CLIENT_ID'))
 
 app = Flask(__name__)
-app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
 app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
+
+app.logger.info(f'SECRET KEY: {os.environ.get('SECRET_KEY')}')
+app.logger.info(f'BLOB ACCOUNT: {os.environ.get('BLOB_ACCOUNT')}')
+app.logger.info(f'BLOB STORAGE KEY: {os.environ.get('BLOB_STORAGE_KEY')}')  
+app.logger.info(f'BLOB CONTAINER: {os.environ.get('BLOB_CONTAINER')}')
+app.logger.info(f'SQL SERVER: {os.environ.get('SQL_SERVER')}') 
+app.logger.info(f'SQL DATABASE: {os.environ.get('SQL_DATABASE')}')
+app.logger.info(f'SQL USER NAME: {os.environ.get('SQL_USER_NAME')}')
+app.logger.info(f'SQL PASSWORD: {os.environ.get('SQL_PASSWORD')}')
+app.logger.info(f'CLIENT SECRET: {os.environ.get('CLIENT_SECRET')}')
+app.logger.info(f'CLIENT ID: {os.environ.get('CLIENT_ID')}')
+
+
+app.config.from_object(Config)
+# TODO: Add any logging levels and handlers with app.logger
+# app.logger.setLevel(logging.WARNING)
+# streamHandler = logging.StreamHandler()
+# streamHandler.setLevel(logging.WARNING)
+# app.logger.addHandler(streamHandler)
 
 Session(app)
 db = SQLAlchemy(app)
