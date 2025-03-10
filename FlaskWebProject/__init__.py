65e715e756320282d7d5cc5ca04b38f1d92513c7
DanielleShine <danielleshineabacial@gmail.com>
2025-03-10 14:13:12 +0800

    printing env var

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,6 +9,17 @@ from flask_login import LoginManager
 from flask_session import Session
 import os
 
+print(os.environ.get('SECRET_KEY'))
+print(os.environ.get('BLOB_ACCOUNT'))
+print(os.environ.get('BLOB_STORAGE_KEY'))
+print(os.environ.get('BLOB_CONTAINER'))
+print(os.environ.get('SQL_SERVER'))
+print(os.environ.get('SQL_DATABASE'))
+print(os.environ.get('SQL_USER_NAME'))
+print(os.environ.get('SQL_PASSWORD'))
+print(os.environ.get('CLIENT_SECRET'))
+print(os.environ.get('CLIENT_ID'))
+
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
